6a9767d090cc7d679dc4cf237d538391dfc00b88
Jiri Furst <Jiri.Furst@fs.cvut.cz>
2016-10-11 11:11:15 +0200

    Improved README and stub for Burgers equation

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # MMPP
-Matematické modelování problémů proudění, přednášky na FS ČVUT v Praze
+
+Materiály pro přednášky *Matematické modelování problémů proudění* na
+Fakultě strojní ČVUT v Praze.
+
+
+## Poznámky k přednáškám a cvičením
+1. Lineární advekce
+   * [poznámky](lessons/01_linearni_advekce.ipynb)
+   * ukázkové programy:
+	 *[schéma upwind](src/linadv_upwind.cpp)
+2. Nelineární Burgersova rovnice
+   * [poznámky](lessons/02_burgersova_rovnice.ipynb)
+   * ukázkové programy:
+	 *[schéma upwind](src/burgers_upwind.cpp)
+
--- src/burgers_upwind.cpp
+++ src/burgers_upwind.cpp
@@ -1,4 +1,4 @@
-/*- Upwind scheme for linear advection */
+/*- Upwind scheme for Burgers equation */
 
 // Copyright 2016 Jiri Furst
 // Distributed under MIT license
@@ -15,8 +15,8 @@ using namespace std;
 //- Initial condition
 double u0(double x) 
 {
-  if (x<0.25 || x>0.5) return 0.0;
-  else return cos( (8*x-1)*M_PI) + 1;
+  if (x<0.25 || x>0.75) return 0.0;
+  else return sin( 2*M_PI*(2*x-1));
 }
 
 //- Save result to a file
@@ -29,12 +29,22 @@ void save_result(const string& filename, const vector<double>& x, const vector<d
     f << x[i] << "\t" << u[i] << endl;
 }
 
+
+double flux(double ul, double ur) {
+  double a = (ul+ur) / 2.;
+  if (a > 0) return ul*ul/2.;
+  if (a < 0) return ur*ur/2.;
+  return 0.;
+}
+
+
 int main()
 {
   const int   nx = 101;         // Number of points 
   const double L = 1.0;         // Length of the domain
   const double a = 1.0;         // Advection speed
-  const double Tend = 0.25;
+  //const double Tend = 0.25;
+  const double Tend = 0.05;
 
   vector<double> x(nx);
   vector<double> u(nx);
@@ -53,12 +63,17 @@ int main()
   dt = Tend / niter;
 
   vector<double> un(nx);
+  vector<double> f(nx+1);
 
   double t = 0;
   for (int n=0; n<niter; n++) {
+    f[0]  = flux(0, u[0]);
+    f[nx] = flux(u[nx-1],0);
+    for (int i=1; i<nx; i++)
+      f[i] = flux( u[i-1], u[i] );
 
-    for (int i=1; i<nx; i++) 
-      un[i] = u[i] - a*dt/dx * (u[i] - u[i-1]);
+    for (int i=0; i<nx; i++) 
+      un[i] = u[i] - dt/dx * (f[i+1] - f[i]);
 
     t += dt;
     for (int i=1; i<nx; i++) u[i] = un[i];
